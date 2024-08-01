bbac70905c56271c646151fcb5df70b5438b351e
Mukul Singh <135323609+Mukul1598@users.noreply.github.com>
2024-08-01 15:32:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+squares.forEach((square) => {
+	square.addEventListener("mouseover", handleMouseOver)
+	square.addEventListener("mouseout", handleMouseOut)
+})
+
+function handleMouseOver(event){
+	squares.forEach((square) => {
+		if(square !== event.target){
+			square.style.backgroundColor = "#6F4E37";
+		}
+	})
+}
+
+function handleMouseOut(event){
+	squares.forEach((square) => {
+		square.style.backgroundColor = "E6E6FA";
+	})
+}
